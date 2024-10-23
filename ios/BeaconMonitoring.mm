083d33c5ae8a7d1c57ffaff25bbc6933a7f0ac64
Joshua Siwek <joshua.siwek@jaemacom.de>
2024-10-23 07:43:34 +0200

    chore: initial commit; init project

--- ios/BeaconMonitoring.mm
+++ ios/BeaconMonitoring.mm
@@ -1,10 +1,15 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
-@interface RCT_EXTERN_MODULE(BeaconMonitoring, NSObject)
+@interface RCT_EXTERN_MODULE(BeaconMonitoring, RCTEventEmitter)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(startMonitoring:(NSString *)uuid
+                  identifier:(NSString *)identifier
+                  resolve:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(stopMonitoring:(RCTPromiseResolveBlock)resolve
+                  reject:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/BeaconMonitoring.swift
+++ ios/BeaconMonitoring.swift
@@ -1,8 +1,59 @@
-@objc(BeaconMonitoring)
-class BeaconMonitoring: NSObject {
+import CoreLocation
+import React
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
+@objc(BeaconMonitoring)
+class BeaconMonitoring: RCTEventEmitter, CLLocationManagerDelegate {
+
+  var locationManager: CLLocationManager?
+  var beaconRegion: CLBeaconRegion?
+
+  override init() {
+    super.init()
+    locationManager = CLLocationManager()
+    locationManager?.delegate = self
+  }
+
+    @objc func startMonitoring(uuid: String, identifier: String, resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+        guard let beaconUUID = UUID(uuidString: uuid) else {
+          reject("BeaconMonitoringError", "Invalid UUID", nil)
+          return
+        }
+
+        beaconRegion = CLBeaconRegion(uuid: beaconUUID, identifier: identifier)
+        if let beaconRegion = beaconRegion {
+          locationManager?.startMonitoring(for: beaconRegion)
+          resolve("Started monitoring for beacon region")
+        } else {
+          reject("BeaconMonitoringError", "Failed to create beacon region", nil)
+        }
+    }
+
+    @objc func stopMonitoring(resolve: @escaping RCTPromiseResolveBlock, reject: @escaping RCTPromiseRejectBlock) {
+    if let beaconRegion = beaconRegion {
+      locationManager?.stopMonitoring(for: beaconRegion)
+      resolve("Stopped monitoring beacon region")
+    } else {
+      reject("BeaconMonitoringError", "No beacon region to stop monitoring", nil)
+    }
+  }
+
+  func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
+    if region is CLBeaconRegion {
+      sendEvent(withName: "didEnterRegion", body: ["identifier": region.identifier])
+    }
+  }
+
+  func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+    if region is CLBeaconRegion {
+      sendEvent(withName: "didExitRegion", body: ["identifier": region.identifier])
+    }
+  }
+
+  override func supportedEvents() -> [String]! {
+    return ["didEnterRegion", "didExitRegion"]
+  }
+
+  override static func requiresMainQueueSetup() -> Bool {
+    return false
   }
 }
